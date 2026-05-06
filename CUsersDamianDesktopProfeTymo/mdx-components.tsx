a6fa57e3cc19cece687815ffa48bd524a2751afe
damitymo <tymoszukdamiandejesus@gmail.com>
2026-05-05 22:54:44 -0300

    primer commit del sitio

--- CUsersDamianDesktopProfeTymo/mdx-components.tsx
+++
@@ -1,8 +0,0 @@
-import type { MDXComponents } from "mdx/types";
-
-// Sobreescribir o agregar componentes para usar dentro de los .mdx
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    ...components,
-  };
-}
